a6d43ea5870fefdf7a21501904e72aa00d4a6470
Antonio Pérez <antonio.perez@digital55.com>
2020-04-25 17:45:40 +0200

    added materialize css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ require("jquery3")
 require("popper")
 require("bootstrap")
 
+import './stylesheets/application'
+import 'materialize-css/dist/js/materialize'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
