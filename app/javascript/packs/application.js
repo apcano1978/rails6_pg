b511e959a7eb3c39b7c236c610ba3db074510057
Antonio Pérez <antonio.perez@digital55.com>
2020-05-03 11:22:16 +0200

    stimulus installed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,20 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery3")
-require("popper")
-require("bootstrap")
 
 import './stylesheets/application'
 import 'materialize-css/dist/js/materialize'
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
-//= require jquery3
-//= require popper
-//= require bootstrap
+import "controllers"
